99d09ccc18229ae04ef72a411e2b4dbd5bc2240e
Evernight <786357+Evernight@users.noreply.github.com>
2025-09-24 22:41:45 +0200

    add generate_base_ccy_prices.py

---
+++ src/beancount_lazy_plugins/generate_base_ccy_prices.py
@@ -0,0 +1,42 @@
+"""A plugin that inserts an additional price to the base rate by applying
+fx rate to a price.
+
+Original can be found at:
+https://github.com/tarioch/beancounttools/blob/master/src/tariochbctools/plugins/generate_base_ccy_prices.py
+"""
+from beancount.core import amount, data, prices
+from collections import defaultdict
+
+__plugins__ = ["generate"]
+
+def generate(entries, options_map, baseCcy):
+    errors = []
+    priceMap = prices.build_price_map(entries)
+    existingDates = defaultdict(None)
+
+    additionalEntries = []
+    for entry in entries:
+        if isinstance(entry, data.Price) and entry.amount.currency != baseCcy:
+            fxTuple = tuple([entry.amount.currency, baseCcy])
+            fxRate = prices.get_price(priceMap, fxTuple, entry.date)
+
+            if not fxRate[1]:
+                continue
+
+            pair = (entry.currency, baseCcy)
+            if pair not in existingDates:
+                existingDates[pair] = set([item[0] for item in priceMap.get(pair, [])])
+            if entry.date in existingDates[pair]:
+                continue
+
+            priceInBaseCcy = amount.Amount(entry.amount.number * fxRate[1], baseCcy)
+
+            additionalEntries.append(
+                data.Price(entry.meta, entry.date, entry.currency, priceInBaseCcy)
+            )
+
+            existingDates[pair].add(entry.date)
+
+    entries.extend(additionalEntries)
+
+    return entries, errors
